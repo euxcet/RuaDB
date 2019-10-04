5c93213fea96127a52a60861a5a86eb071321552
euxcet <zcc.qwer@gmail.com>
2019-10-04 18:05:33 +0800

    use lalrpop to parse sql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#![allow(unreachable_patterns)]
+
+// #[macro_use]
+extern crate lalrpop_util;
+
+pub mod parser;
+pub mod ast;
+pub mod tok;
+
+pub type ParseError<'input> = lalrpop_util::ParseError<usize, tok::Tok<'input>, tok::Error>;
+
+
+pub fn parse_sql<'input>(input: &'input str) -> Result<Vec<Option<ast::Cmd>>, ParseError<'input>> {
+    let tokenizer = tok::Tokenizer::new(input, 0);
+    let sql = parser::lrsql::CmdListParser::new().parse(input, tokenizer)?;
+    Ok(sql)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let res = parse_sql("CREATE TABLE test (col)").unwrap();
+    for i in &res {
+        match i {
+            Some(cmd) => { // cmd ast::Cmd
+                match cmd {
+                    ast::Cmd::Stmt(x) => { // ast::Stmt
+                        match x {
+                            ast::Stmt::CreateTable{ tbl_name, .. } => {
+                                println!("{}", tbl_name.name);
+                            },
+                            _ => {},
+                        }
+                    },
+                    ast::Cmd::Explain(x) => println!("this is a explain"),
+                    ast::Cmd::ExplainQueryPlan(x) => println!("this is a explain query plan"),
+                }
+            },
+            None => println!("None"),
+        }
+    }
 }
